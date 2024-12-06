871f1863fc2be004cdc0f4dd289e48fffce1fb34
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-12-07 00:45:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import './plugins/main-allfake.js';
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botnumber = '' //Ejemplo: +573138954963
+global.botnumber = '+917086726371' //Ejemplo: +573138954963
 global.confirmCode = ''
 
 //• ↳𝑺𝑶𝑳𝑶 𝑫𝑬𝑺𝑨𝑹𝑹𝑶𝑳𝑳𝑨𝑫𝑶𝑹𝑬𝑺 𝑨𝑷𝑹𝑶𝑩𝑨𝑫𝑶𝑺
